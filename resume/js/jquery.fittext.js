1a5d3f58e2845fdec9ba7108b305748b9a07daf7
longzhi <long.dizhi@gmail.com>
2020-03-15 14:39:36 +0800

    Site updated: 2020-03-15 14:39:26

---
+++ resume/js/jquery.fittext.js
@@ -0,0 +1,3 @@
+// build time:Sun Mar 15 2020 14:38:34 GMT+0800 (GMT+08:00)
+!function(t){t.fn.fitText=function(n,i){var e=n||1,o=t.extend({minFontSize:Number.NEGATIVE_INFINITY,maxFontSize:Number.POSITIVE_INFINITY},i);return this.each(function(){var n=t(this),i=function(){n.css("font-size",Math.max(Math.min(n.width()/(10*e),parseFloat(o.maxFontSize)),parseFloat(o.minFontSize)))};i(),t(window).on("resize.fittext orientationchange.fittext",i)})}}(jQuery);
+//rebuild by neat 
