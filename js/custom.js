89e197543d46bba151c855d9e2efa3290782468e
longzhi <long.dizhi@gmail.com>
2020-10-26 23:39:39 +0800

    Site updated: 2020-10-26 23:39:21

---
+++ js/custom.js
@@ -0,0 +1,8 @@
+/*去掉 Banner 图*/
+var full_page = document.getElementsByClassName("full_page");
+if (full_page.length != 0) {
+  full_page[0].style.background = "transparent";
+}
+/* 页脚养鱼*/
+var xiaokang = new xkTool();
+xiaokang.footFish();
